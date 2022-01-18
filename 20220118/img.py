ca03b92fb14992b05b296a58115a2fbac772d32e
flt6 <1404262047@qq.com>
2022-01-18 17:34:22 +0800

    Changes to be committed: 	new file:   Accelerometer.csv 	new file:   FAccelerometer.csv 	new file:   FGyroscope.csv 	new file:   Gyroscope.csv 	new file:   img.py 	new file:   main.cpp 	new file:   out.txt 	new file:   transfer.py

---
+++ 20220118/img.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+from numpy import  array
+x=[]
+y=[]
+with open("out.txt","r") as f:
+    for i in f:
+        t1,t2=i.split(" ")
+        x.append(float(t1))
+        y.append(float(t2))
+plt.title("X-Y")
+len=max(max(x)-min(x),max(y)-min(y))+4
+plt.xlim((min(x)-2,min(x)+len))
+plt.ylim((min(y)-2,min(y)+len))
+plt.plot(x, y,'.')
+plt.grid()
+# plt.savefig("img.png",dpi=2000)
+plt.show(block=True)
+
