1ab030de8a0894b8f6e3b44f5efd2a84dfa085bc
flt6 <1404262047@qq.com>
2022-01-19 21:31:38 +0800

    Write Code README in 20220119_1 Write Expriment README in 20220119_2 rebuild image in 202220118

--- 20220118/img_sav.py
+++ 20220118/img_sav.py
@@ -13,24 +13,14 @@ with open("FAccelerometer.csv") as f:
     for i in f:
         t1,_,_,_=i.split()
         t.append(float(t1))
+t.pop()
+# To avoid Error because of Bug in main.cpp#23
 plt.figure(figsize=(5,5))
 plt.title("X-Y")
 len1=max(max(x)-min(x),max(y)-min(y))+0.1
 plt.xlim((min(x)-0.05,min(x)+len1))
 plt.ylim((min(y)-0.05,min(y)+len1))
 plt.grid()
+plt.plot(x,y,".")
 
-# plt.savefig("img.png",dpi=2000)
-
-
-t[0]=0.001
-for i in range(len(t)):
-    dt=t[i]-t[i-1]
-    if dt<=0:dt=0.001
-    # print(dt)
-    plt.plot(x[i], y[i],'.',color="black")
-    plt.pause(dt)
-
-
-# plt.show(block=True)
-plt.pause(0)
+plt.savefig("img.png",dpi=800)
